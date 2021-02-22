8f4e3b7beacb4515ea6a51c02e1ba356c982e2c8
ismetalp98 <alp.eren@ug.bilkent.edu.tr>
2021-02-22 14:53:33 +0300

    1 binaries uploaded

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
+import background from "./img/asd";
 
 class Header extends Component {
   render() {
     return (
-      <div className=" header col-xs-12">
+      <div
+        className=" header col-xs-12"
+        style={{ backgroundImage: `url(${background})` }}
+      >
         <p className="intro col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-3">
           <span>Otsimo Word Counter</span>
         </p>
